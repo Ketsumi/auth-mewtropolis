e8e89f93963e8bcf2adeddca05e34f21d9d84130
ketsumi <mewtropolis.ketsumi@gmail.com>
2019-06-02 01:16:52 -0400

    login & callback components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CallbackComponent } from './callback/callback.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CallbackComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
